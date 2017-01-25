6b2f1fd14245951842dfa6ff92d9ac1b34d113b2
Matthew Leeds <mwl458@gmail.com>
2017-01-24 21:54:27 -0600

    Automatically rename files with the current date

--- make_resumes.sh
+++ make_resumes.sh
@@ -3,7 +3,10 @@
 set -x
 
 rm -f *.pdf *.aux *.log
+
 pdflatex Matthew_Leeds_*_CV.tex
 pdflatex Matthew_Leeds_*_work.tex
 pdflatex Matthew_Leeds_*_research.tex
 pdflatex Matthew_Leeds_*_volunteer.tex
+
+rename "s/\d\d\d\d-\d\d-\d\d/`date --iso-8601`/" *.pdf *.tex
